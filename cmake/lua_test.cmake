e9ad9b4d66b64688bf61cc125c6e74b60414619a
Alex Ames <alex@agentic.ai>
2025-02-20 19:49:52 -0800

    wip

--- cmake/lua_test.cmake
+++ cmake/lua_test.cmake
@@ -1,5 +1,20 @@
-function(add_lua_test test_name)
+function(add_lua_test test_name lua_file)
+    if (LUA_BUILD_TESTS)
+        add_custom_target(test_${test_name}
+            COMMAND
+                "$<TARGET_FILE:${LUA_INTERPRETER_TARGET}>" 
+                "${LUA_SOURCE_DIR}/testes/${lua_file}"
+            WORKING_DIRECTORY
+                "${LUA_SOURCE_DIR}/testes"
+            SOURCES
+                "${LUA_SOURCE_DIR}/testes/${lua_file}"
+            DEPENDS
+                "${LUA_SOURCE_DIR}/testes/${lua_file}"
+            USES_TERMINAL
+        )
+        add_dependencies(test_${test_name}
+            ${LUA_INTERPRETER_TARGET}
+        )
+    endif()
+endfunction()
 
-    add_custom_target(
-
-endfunction
